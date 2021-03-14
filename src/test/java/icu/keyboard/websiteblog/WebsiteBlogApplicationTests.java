db21eaa447dd912c8748533f5053cdf5b8bf082a
dskbrd <keyboard@kbrd.pw>
2021-03-14 23:02:31 +0100

    Init: basic project setup with spring and vaaddin starters

---
+++ src/test/java/icu/keyboard/websiteblog/WebsiteBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.keyboard.websiteblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
